c8802aab5018e5d07fc270a0eee3e71fe332ae14
Colin Su <littleq0903@gmail.com>
2016-04-13 17:16:16 +0600

    set origin to all and accepts connection from anywhere

--- AlphaGo/server.py
+++ AlphaGo/server.py
@@ -6,11 +6,15 @@ from bottle import request, route
 import go
 
 @route('/move', method='POST')
-def move(self):
+def move():
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
+
     incoming_bundle = request.json
 
     return go.do_workflow(incoming_bundle)
 
 if __name__ == "__main__":
     # TODO: initialize your model here
-    bottle.run()
+    bottle.run(host='0.0.0.0')

0168fb93fbd2d7c13be7cbea62e22731b0eac394
Colin Su <littleq0903@gmail.com>
2016-04-16 22:04:13 +0600

    clean up the server code, leave the game state manager pure

--- AlphaGo/server.py
+++ AlphaGo/server.py
@@ -1,36 +1,39 @@
+#!/usr/bin/env python
+# standard libraries
+import os
 import json
-
-import bottle
-from bottle import request, response, route
-
-import go
-
 import uuid
 
+# relevant third-party libraries
+import bottle
+from bottle import request, response, route
+from keras.callbacks import ModelCheckpoint
+
+# local modules
+from AlphaGo.models.policy import CNNPolicy
+import go
+
+# global variables
 DEBUG = True
 GSM_POOL = dict()
-GSM_IP_POOL = dict()
-import os
-import argparse
-import json
-#import cPickle as pickle
-import six.moves.cPickle as pickle
-import random
-import numpy as np
-from keras.callbacks import ModelCheckpoint
-from AlphaGo.models.policy import CNNPolicy
+POLICY = None
 
-#def __init__(self, *args, **kwargs):
-train_folder = '/home/yimlin/betago_workspace/deploy'
-metapath = os.path.join(train_folder, 'all_feat_model.json')
-with open(metapath) as metafile:
-    metadata = json.load(metafile)
-weights_file= os.path.join(train_folder, 'weights.3rdEpoch.hdf5');
-arch = {'filters_per_layer': 128, 'layers': 12} # args to CNNPolicy.create_network()
-policy = CNNPolicy(feature_list=metadata['feature_list'], **arch);
-policy.model.load_weights(weights_file);
-policy.model.compile(loss='categorical_crossentropy', optimizer='sgd')
-#bottle.__init__(self, *args, **kwargs)
+
+def init_cnnpolicynetwork():
+    global POLICY
+
+    train_folder = '/home/yimlin/betago_workspace/deploy'
+    metapath = os.path.join(train_folder, 'all_feat_model.json')
+
+    with open(metapath) as metafile:
+        metadata = json.load(metafile)
+
+    weights_file = os.path.join(train_folder, 'weights.3rdEpoch.hdf5')
+    arch = {'filters_per_layer': 128, 'layers': 12} # args to CNNPolicy.create_network()
+    POLICY = CNNPolicy(feature_list=metadata['feature_list'], **arch)
+    POLICY.model.load_weights(weights_file)
+    POLICY.model.compile(loss='categorical_crossentropy', optimizer='sgd')
+
 
 @route('/move', method='OPTIONS')
 @route('/move', method='POST')
@@ -42,44 +45,32 @@ def move():
     if request.method == 'OPTIONS':
         return 'received options request'
 
-    if request.get_cookie('betago_user_guid'):
-        player_uuid = request.get_cookie('betago_user_guid')
-    else:
-        player_uuid = str(uuid.uuid4())
-        response.set_cookie('betago_user_guid', player_uuid)
-
-    if DEBUG:
-        print 'input: ', str(request.body.read())
-        print 'size_gsm_pool: ', len(GSM_POOL)
-
     incoming_bundle = request.json
-    client_ip = request.environ.get('REMOTE_ADDR')
 
-    sessionId = ""
-    if ('sessionId' in incoming_bundle):
-        sessionId = incoming_bundle['sessionId']
-        if (sessionId ==""):
-            sessionId = str(uuid.uuid4())
-            incoming_bundle['sessionId'] = sessionId
-
-    if (sessionId ==""):
-        sessionId = client_ip;
-        incoming_bundle['sessionId'] = sessionId
-
-    if sessionId not in GSM_IP_POOL:
-        gsm = go.GameStateManager(policy)
-        GSM_IP_POOL[sessionId] = gsm
+    # determine what session id to use
+    if ('sessionId' in incoming_bundle and incoming_bundle['sessionId']):
+        session_id = incoming_bundle['sessionId']
     else:
-        gsm = GSM_IP_POOL[sessionId]
+        session_id = str(uuid.uuid4())
+
+    # retrieve or create GameStateManager
+    if session_id not in GSM_POOL:
+        gsm = go.GameStateManager(POLICY)
+        GSM_POOL[session_id] = gsm
+    else:
+        gsm = GSM_POOL[session_id]
 
     resp = gsm.do_workflow(incoming_bundle)
+    resp['sessionId'] = session_id
 
     if DEBUG:
         gsm.print_board()
+        print 'input: ', str(request.body.read())
+        print 'size_gsm_pool: ', len(GSM_POOL)
 
     return resp
 
+
 if __name__ == "__main__":
-    # TODO: initialize your model here
-    
+    init_cnnpolicynetwork()
     bottle.run(host='0.0.0.0', port=8080, debug=True)
